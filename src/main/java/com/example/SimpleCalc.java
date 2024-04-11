b2751563d989e7379f13a3aa8025fd077c5356b3
e-albaugh <eric.albaugh@microfocus.com>
2024-04-11 17:12:06 -0500

    think that worked. fixing pom

--- src/main/java/com/example/SimpleCalc.java
+++ src/main/java/com/example/SimpleCalc.java
@@ -1,6 +1,11 @@
 package com.example;
 
+
 public class SimpleCalc {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+    
     public int add(int a, int b) {
         return a + b;
     }
