c2c14a4cc6d25eb7a9f70be33c4b55035ea73641
e-albaugh <eric.albaugh@microfocus.com>
2024-04-11 16:38:55 -0500

    Initial commit

---
+++ src/test/java/com/example/SimpleCalculatorTest.java
@@ -0,0 +1,59 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SimpleCalculatorTest {
+    private final SimpleCalculator calculator = new SimpleCalculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(-1, calculator.subtract(2, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calculator.divide(6, 3));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivisionByZero() {
+        calculator.divide(1, 0);
+    }
+
+    // Five more tests that vary the inputs or methods to ensure full coverage
+    @Test
+    public void testAddNegativeNumbers() {
+        assertEquals(-5, calculator.add(-2, -3));
+    }
+
+    @Test
+    public void testMultiplyWithZero() {
+        assertEquals(0, calculator.multiply(0, 3));
+    }
+
+    @Test
+    public void testSubtractToZero() {
+        assertEquals(0, calculator.subtract(3, 3));
+    }
+
+    @Test
+    public void testDivideNegative() {
+        assertEquals(-2, calculator.divide(6, -3));
+    }
+
+    @Test
+    public void testMultiplyNegatives() {
+        assertEquals(6, calculator.multiply(-2, -3));
+    }
+}
