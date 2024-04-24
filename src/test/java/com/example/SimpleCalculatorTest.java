57e1f9783f6f6e1969ea16feb67aaaa143c34ddc
e-albaugh <51415624+e-albaugh@users.noreply.github.com>
2024-04-24 14:58:21 -0400

    add fail error log

--- src/test/java/com/example/SimpleCalculatorTest.java
+++ src/test/java/com/example/SimpleCalculatorTest.java
@@ -35,6 +35,7 @@ public class SimpleCalculatorTest {
     @Test
     public void testMultiplyWithZero() {
         assertEquals(0, calculator.multiply(0, 3));
+        fail("Multiply by zero not yet implemented");
     }
 
     @Test
